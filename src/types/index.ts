5b4e9e497129b238977b13b4f47536c646e04db6
Ekaterina <solovyovaalw@gmail.com>
2025-01-20 02:43:20 +0300

    initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,39 @@
+export interface IProductItem {
+    id: string;
+    description?: string;
+    img?: string;
+    title: string;
+    category?: string;
+    price: number | null;   
+}
+
+export interface IOrder {
+    paymentMethod: 'Онлайн' | 'При получении';
+    deliveryAddress: string;
+    customerEmail: string;
+    customerPhone: string;
+}
+
+export interface IAppState {
+    catalog: TProductItemCard[];
+    basket: string[];
+    preview: string | null;
+    order: IOrder | null;
+
+    toggleOrderedProduct(id: string, isIncluded: boolean): void;
+    clearBasket(): void;
+    getTotal(): number;
+    setCatalog(items: TProductItemCard): void;
+    setPreview(item: IProductItem): void;
+    setOrderField<T extends keyof IOrder>(field: T, value: IOrder[T]): void;
+    checkValidation(): boolean;
+}
+
+export type TProductItemCard = Pick<IProductItem, 'id' | 'category' | 'title' | 'img' | 'price'>;
+
+export type IOrderContactForm = Pick<IOrder, 'customerEmail' | 'customerPhone'>;
+export type IOrderPayAndAdressForm = Pick<IOrder, 'paymentMethod' | 'deliveryAddress'>;
+ 
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export type IBusketItem = Pick<IProductItem, 'id' | 'title' | 'price'>;
